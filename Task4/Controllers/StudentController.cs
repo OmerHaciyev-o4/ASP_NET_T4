d32994075e5d5b9a1fe7bba4612e0128d3215112
OmerHaviyev-o4 <omer04118102@gmaiil.com>
2022-04-06 12:27:57 +0400

    V1

---
+++ Task4/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task4.Repos;
+
+namespace Task4.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Home()
+        {
+            ViewBag.Students = StudentRepo.Students;
+            return View();
+        }
+
+        [HttpPost]
+        public string Details(string info = null)
+        {
+            return info.ToString();
+        }
+    }
+}
---
+++ Task4/Entities/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Task4.Entities
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string URL { get; set; }
+    }
+}
